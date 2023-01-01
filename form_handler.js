746746eb49be8f0126d36a0ef3a1ec1e352226bb
Teppi Le <teppipersonal@gmail.com>
2023-01-01 14:55:52 +0700

    Update form_handler.js

--- form_handler.js
+++ form_handler.js
@@ -12,4 +12,4 @@ function handleSubmit(event) {
   
 const form = document.getElementById('form');
 const log = document.getElementById('log');
-form.addEventListener('submit', logSubmit);
+form.addEventListener('submit', handleSubmit);
