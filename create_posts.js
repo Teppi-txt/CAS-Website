50f64836e6278e155f7445872160e33b0f0593fd
Teppi Le <teppipersonal@gmail.com>
2023-01-01 22:48:31 +0700

    s

--- create_posts.js
+++ create_posts.js
@@ -5,10 +5,40 @@ fetch('./data.json')
     }));    
 
 const content = document.getElementById("content");
+var colorDict = {
+    "general" : "#E9A641",
+    "english lang & lit": "#BB38E9",
+    "english lit": "#BB38E9",
+    "spanish" : "#EB6C6C",
+    "french" : "#EB6C6C",
+    "vietnamese" : "#EB6C6C",
+    "self taught" : "#EB6C6C",
+    "mandarin" : "#EB6C6C",
+    "economics" : "#FDDC66",
+    "business" : "#FDDC66",
+    "psychology" : "#FDDC66",
+    "history" : "#FDDC66",
+    "geography" : "#FDDC66",
+    "computer science" : "#5ABDC0",
+    "physics" : "#5ABDC0",
+    "chemistry" : "#5ABDC0",
+    "biology" : "#5ABDC0",
+    "design" : "#5ABDC0",
+    "sports science" : "#5ABDC0",
+    "environmental science" : "#5ABDC0",
+    "math aa" : "#3B6DED",
+    "math ai" : "#3B6DED",
+    "tok" : "#E077F1",
+    "cas" : "#E077F1",
+    "ee" : "E077F1"
+}
 
 
-function createElement(subject, date, text) {
-    var post = document.createElement("div")
-    post.innerHTML = '<div class="post" id="post"><div class="header"><h1 class="subject-display">' + subject + '</h1><h2 class="date">' + date + '</h2></div><p class="text">' + text + '</p></div>';
+function createElement(subject, text, date) {
+    var post = document.createElement("div");
+    var parsedDate = date.substring(0, 2) + " / " + date.substring(2, 4) + " / 20" + date.substring(4, 6);
+    post.className = "post";
+    post.innerHTML = '<div class="header"><h1 class="subject-display">' + subject + '</h1><h2 class="date">' + parsedDate + '</h2></div><p class="text">' + text + '</p>';
+    post.style.backgroundColor = colorDict[subject.toLowerCase()];
     content.append(post);
 }
--- form_handler.js
+++ form_handler.js
@@ -1,15 +1,25 @@
-function handleSubmit(event) {
-    event.preventDefault();
-  
-    const data = new FormData(event.target);
-  
-    const value = data.get('text');
-  
-    console.log({ value });
-    log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-    event.preventDefault();
+function handleSubmit(subject, text) {
+    var currentJson = [];
+    fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => json.forEach(element => {
+        currentJson.push(element)
+    }));
+
+
+    const d = new Date();
+    currentJson.push([subject.value, text.value, d.getDate() + d.getMonth() + "23"]);
+    console.log(currentJson);
+    var json = JSON.stringify(currentJson);
+    writeFile('./data.json', json, 'utf8', callback);
 }
   
-const form = document.getElementById('form');
-const log = document.getElementById('log');
-form.addEventListener('submit', handleSubmit);
+
+
+import { writeFile } from 'fs';
+const postBtn = document.getElementById('post');
+const subject = document.getElementById('subject');
+const text = document.getElementById('text');
+postBtn.onclick = function() {
+    handleSubmit(subject, text)
+};
