72a2d1cbf5ccb9f2e10c2ceb48fac0c6ef02bfff
Teppi Le <teppipersonal@gmail.com>
2023-01-01 14:35:46 +0700

    Update form_handler.js

--- form_handler.js
+++ form_handler.js
@@ -1,10 +1,7 @@
 function handleSubmit(event) {
     event.preventDefault();
-  
     const data = new FormData(event.target);
-  
     const value = data.get('email');
-  
     console.log({ value });
 }
   
